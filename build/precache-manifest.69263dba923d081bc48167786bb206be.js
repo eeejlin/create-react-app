b67925616c90f95d51a58ecd9c15c09040071181
EeeejLin <114950700+eeejlin@users.noreply.github.com>
2022-10-19 22:54:11 -0400

    V1

---
+++ build/precache-manifest.69263dba923d081bc48167786bb206be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c808b591cbbcd6d382b",
+    "url": "/create-react-app/static/css/main.1cbc3547.chunk.css"
+  },
+  {
+    "revision": "8c808b591cbbcd6d382b",
+    "url": "/create-react-app/static/js/main.c0fd6114.chunk.js"
+  },
+  {
+    "revision": "bc3d0128b3768e33ec05",
+    "url": "/create-react-app/static/js/runtime~main.f2e45a83.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/create-react-app/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/create-react-app/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/create-react-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1806f26df7e2e95a8cabf50704b45c9c",
+    "url": "/create-react-app/index.html"
+  }
+];
